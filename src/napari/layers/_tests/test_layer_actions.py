02c65c8e37d6457be1666d94177500a894143e95
Carlos Rodriguez <carlosmariorr90@hotmail.com>
2026-09-07 22:46:15 +0900

    Added extract multiscale level test

--- src/napari/layers/_tests/test_layer_actions.py
+++ src/napari/layers/_tests/test_layer_actions.py
@@ -20,6 +20,7 @@ from napari.layers._layer_actions import (
     _split_stack,
     _toggle_lock,
     _toggle_visibility,
+    extract_multiscale_level,
 )
 from napari.utils.transforms import Affine
 
@@ -504,3 +505,22 @@ def test_zarr_projection_is_lazy(mode):
             projected.compute(),
             getattr(np, mode)(original, axis=projecting_axis),
         )
+
+
+@pytest.mark.parametrize('layer_type', [Image, Labels])
+@pytest.mark.parametrize(('level', 'expected_level'), [(0, 0), (None, 1)])
+def test_extract_multiscale_level(layer_type, level, expected_level):
+    layer_name = 'test_layer'
+    data = (np.zeros((16, 16), dtype=int), np.zeros((8, 8), dtype=int))
+    layer = layer_type(data, name=layer_name)
+    layer.data_level = 1
+
+    new_layer = extract_multiscale_level((layer,), level=level)[0]
+
+    np.testing.assert_array_equal(new_layer.data, data[expected_level])
+    assert not new_layer.multiscale
+    assert new_layer.name == f'{layer_name}-level({expected_level})'
+    np.testing.assert_array_equal(
+        new_layer.scale,
+        np.asarray(layer.scale) * layer.downsample_factors[expected_level],
+    )
